4df1b4b39625cbf2371ec8630c76c0aaa9fe23ba
Apple <sarah.crawley6@gmail.com>
2016-03-09 21:35:48 +0000

    it displays search results test working

--- js/controllers/githubUserSearchController.js
+++ js/controllers/githubUserSearchController.js
@@ -1,5 +1,20 @@
-githubUserSearch.controller('GithubUserSearchController', function() {
-   var self = this;
+githubUserSearch.controller('GithubUserSearchController', [function() {
+  var self = this;
 
-   self.count = 0;
-});
+  self.doSearch = function(){
+    self.searchResult = {
+    items: [
+      {
+        "login":"sarah crawley",
+        "avator_url": "https://avatars0.githubusercontent.com/u/14345034?v=3&s=100",
+        "html_url": "https://github.com/sara6"
+      },
+      {
+        "login":"constantin kalinin",
+        "avator_url":"https://avatars1.githubusercontent.com/u/16108959?v=3&s=100",
+        "html_url":"https://github.com/tishayaem"
+      }
+    ]
+  };
+};
+}]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,5 +1,5 @@
 describe('GithubUserSearchController', function() {
-  
+
   var ctrl;
 
   beforeEach(function(){
@@ -13,5 +13,26 @@ describe('GithubUserSearchController', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
+
+  describe('when searching for a user', function() {
+    var items = [
+      {
+        "login":"sarah crawley",
+        "avator_url":"https://avatars0.githubusercontent.com/u/14345034?v=3&s=100",
+        "html_url":"https://github.com/sara6"
+      },
+      {
+        "login":"constantin kalinin",
+        "avator_url":"https://avatars1.githubusercontent.com/u/16108959?v=3&s=100",
+        "html_url":"https://github.com/tishayaem"
+      }
+    ];
+    
+      it('displays search results', function() {
+        ctrl.searchTerm = 'hello';
+        ctrl.doSearch();
+        expect(ctrl.searchResult.items).toEqual(items);
+        });
+    });
 
 });
